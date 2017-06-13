f8f6cd88c0fba87d29fa572513164aa3f6cd2c6a
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-13 12:50:07 -0400

    post data to firebase server

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,12 @@
     },
     methods: {
       submit() {
-        console.log(this.user);
+        this.$http.post('https://vuejs-http-7f41c.firebaseio.com/data.json', this.user)
+          .then(response => {
+            console.log(response)
+          }, error => {
+            console.log(error);
+          })
       }
     }
   }
